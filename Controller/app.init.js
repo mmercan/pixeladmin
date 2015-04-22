f794fd2857d8a4ebe5b432c749c19bcfaf2784c3
mmercan <mmercan@outlook.com>
2015-04-22 19:00:06 +1000

    additional files for testing the package

---
+++ Controller/app.init.js
@@ -0,0 +1,74 @@
+﻿
+(function () {
+    'use strict';
+
+
+    angular.module('app', ['ngRoute', 'component', 'directives', 'data']);
+  
+
+    angular.module('app').config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+        $routeProvider
+        .when('/test', { templateUrl: '/App/templates/tests.html', controller: 'testCtrl' })
+        .when('/test/:id', { templateUrl: '/App/templates/test.html', controller: 'testCtrl' })
+        .when('/preferences', { templateUrl: '/App/templates/preferences.html', controller: 'PreferencesCtrl' })
+            .otherwise({ redirectTo: '/test' });
+
+
+
+        $routeProvider
+        .when('/charts', { templateUrl: '/App/templates/PixelAdmin/charts.html', controller: 'testCtrl' })
+        .when('/completeui', { templateUrl: '/App/templates/PixelAdmin/completeui.html', controller: 'testCtrl' })
+        .when('/formcomponents', { templateUrl: '/App/templates/PixelAdmin/formComponents.html', controller: 'testCtrl' })
+        .when('/formsadvanced', { templateUrl: '/App/templates/PixelAdmin/formsadvanced.html', controller: 'testCtrl' })
+        .when('/layouts', { templateUrl: '/App/templates/PixelAdmin/layouts.html', controller: 'testCtrl' })
+        .when('/pages1', { templateUrl: '/App/templates/PixelAdmin/pages1.html', controller: 'testCtrl' })
+        .when("/elements", { templateUrl: '/App/templates/PixelAdmin/elements.html', controller: 'testCtrl' })
+        .when('/pagessignup', { templateUrl: '/App/templates/PixelAdmin/pagessignup.html', controller: 'testCtrl' })
+        .when('/pagessignin', { templateUrl: '/App/templates/PixelAdmin/pagessignin.html', controller: 'testCtrl' })
+        .when('/index', { templateUrl: '/App/templates/PixelAdmin/index.html', controller: 'testCtrl' })
+        .when('/test', { templateUrl: '/App/templates/tests.html', controller: 'testCtrl' })
+        .when('/test/:id', { templateUrl: '/App/templates/test.html', controller: 'testCtrl' })
+        .when('/angularcomponents', { templateUrl: '/App/templates/angularcomponents.html', controller: 'testCtrl' })
+        .when('/rottentomatoes', { templateUrl: '/App/templates/PixelAdmin/RottenTomatoes.html', controller: 'rottentomatoesCtrl' });
+    }]);
+
+    angular.module('app').run(["storage", "$rootScope", "$route", "$location", "mainMenuService", function (storage, $rootScope, $route, $location, mainMenuService) {
+
+        var initStorage = function (storageitem) {
+            storageitem.getitems();
+            if (!storageitem.result.listcolumns) {
+                storageitem.result.listcolumns = ["Name"];
+                storageitem.result.viewMode = 'list';
+                storageitem.setrootItem(storageitem.result);
+            }
+        }
+
+
+        var productStorage = new storage("ProductsView");
+        var cultureStorage = new storage("CultureView");
+        var locStorage = new storage("LocationView");
+        initStorage(productStorage);
+        initStorage(cultureStorage);
+        initStorage(locStorage);
+
+
+        //Main Menu items
+        mainMenuService.init();
+        var setactive = function (address) {
+            $("#main-menu  li").removeClass("active");
+            $("#top-menu li").removeClass("active");
+
+            var addr = "a[href='/" + address + "']";
+            var elem = $(addr).parent().addClass("active");
+        }
+        $rootScope.$on('$locationChangeStart', function (ev, next, current) {
+            var nextPath = $location.path();
+            var nextRoute = $route.routes[nextPath];
+
+            setactive("#" + nextPath);
+        });
+    }]);
+
+
+
+})();
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+﻿// Project Json Packages
+//"gulp": "^3.8.11",
+//"gulp-concat": "^2.5.2",
+//"gulp-less": "^3.0.2",
+//"gulp-rename": "^1.2.2",
+//"gulp-inject": "^1.2.0",
+//"gulp-uglify": "^1.2.0",
+//"gulp-cssmin": "^0.1.6"
+
+
+
+var gulp = require("gulp");
+var less = require("gulp-less");
+var concat = require("gulp-concat");
+
+gulp.task("less", function() {
+  return  gulp.src("./wwwroot/lib/pixeladminless/AdminBundles.less")
+        .pipe(less())
+        .pipe(gulp.dest("./wwwroot/lib/pixeladminless/"));
+});
+
+gulp.task('thirdparty', function () {
+    var rootloc = "wwwroot/lib/";
+    var files = [
+        rootloc + "angular/angular.js", rootloc + "angular-route/angular-route.js", rootloc + "angular-animate/angular-animate.js",
+        rootloc + "jquery/jquery.js", rootloc + "jquery-ui.js", rootloc + "jqueryui-touch-punch/jquery.ui.touch-punch.min.js",
+        rootloc + "bootstrap/js/bootstrap.js", rootloc + "respond/respond.src.js",
+        rootloc + "toastr/toastr.js", rootloc + "nprogress/nprogress.js", rootloc + "lodash/lodash.js",
+        rootloc + "moment/moment.js", rootloc + "fullcalendar/fullcalendar.js",
+        rootloc + "jquery-knob/jquery.knob.js", rootloc + "select2/select2.js",
+        rootloc + "jquery.maskedinput/jquery.maskedinput.js",
+        "~/Scripts/bootstrap-datepicker.js",
+        "~/Scripts/jquery.barcode/jquery-barcode.min.js",
+        rootloc + "Dialog/dialog.js",
+        "~/Scripts/kendo/kendo.dataviz.min.js"
+    ];
+
+    return gulp.src(files)
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest("wwwroot/bundle"));
+
+});
